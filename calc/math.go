32e8ba7d3f085de79b469dfc30bdb8a2fd8b7999
Mohit Munjal <mohitmunjal19@gmail.com>
2020-09-06 23:08:34 +0530

    update calc.go

--- calc/math.go
+++ calc/math.go
@@ -1,4 +1,4 @@
-//This signifies that math.go belongs to calc package
+//Package calc This signifies that math.go belongs to calc package
 //since this package is in a seperate directory under out module
 //package declaration has nothing to do with module name
 package calc
