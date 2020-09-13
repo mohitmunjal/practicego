0e8be2014eb4f3af245e47c32dada251eb59e8f1
Mohit Munjal <mohitmunjal19@gmail.com>
2020-09-13 12:18:26 +0530

    update transform.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mohitmunjal/practicego/v2
 
 go 1.14
+
+require github.com/mohitmunjal/practicego v1.0.1
---
+++ transform/program.go
@@ -0,0 +1,11 @@
+package transform
+
+import (
+	"fmt"
+
+	"github.com/mohitmunjal/practicego/calc"
+)
+
+func someFunc() {
+	fmt.Println(calc.Add(1, 2))
+}
